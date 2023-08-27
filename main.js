d9fd04548c22f1ea3cccc3d4054a9262746cb59a
Ferney <ferney.montoyag@gmail.com>
2023-08-27 16:46:49 -0500

    Primera implementación gráfica (solo se completó la parte gráfica para mobile)

--- main.js
+++ main.js
@@ -1,12 +1,74 @@
 const input = document.getElementById('input');
 const btn = document.querySelector('button')
-const p = document.querySelector('.response');
+const cardsContainer = document.querySelector('.cards-container');
 
 const API_KEY = 'a1c39d495474748845de524223a464a4';
 
 let city;
 let latitude;
 let longitude;
+
+{/* <article class="card">
+            <div class="card--city-container">
+                <p>Bucaramanga <span>CO</span> </p>
+                <img src="./assets/close.png" alt="close icon card">
+            </div>
+            
+            <div class="card--temperature-container">
+                <p>28</p>
+                <span>°C</span>
+            </div>
+            
+            <img class="card--img-weather" src="./assets/img-prueba.png" alt="card image weather">
+            
+            <p class="card--weather-description">nubes dispersas</p>
+        </article> */}
+const buildCard = (temperature, cityName, cityCountry, imgIcon, cityWeatherDescription) => {
+    const card = document.createElement('article');
+    card.className = 'card';
+    cardsContainer.appendChild(card)
+
+    const cardCityContainer = document.createElement('div');
+    cardCityContainer.className = 'card--city-container';
+    card.appendChild(cardCityContainer);
+
+    const pCardCityContainer = document.createElement('p');
+    pCardCityContainer.innerText = cityName;
+
+    const spanCardCityContainer = document.createElement('span');
+    spanCardCityContainer.innerText = cityCountry;
+    pCardCityContainer.appendChild(spanCardCityContainer);
+
+    const cityAndCountryContainer = document.createElement('div')
+    cityAndCountryContainer.style = 'display:flex';
+    cardCityContainer.appendChild(cityAndCountryContainer)
+    cityAndCountryContainer.append(pCardCityContainer, spanCardCityContainer)
+
+    const imgCardCityContainer = document.createElement('img');
+    imgCardCityContainer.src = './assets/close.png'
+    imgCardCityContainer.alt = 'close icon card';
+    cardCityContainer.append(imgCardCityContainer);
+
+    const cardTemperatureContainer = document.createElement('div'),
+          pCardTemperatureContainer = document.createElement('p'),
+          spanCardTemperatureContainer = document.createElement('span');
+    cardTemperatureContainer.className = 'card--temperature-container';
+    card.appendChild(cardTemperatureContainer);
+    
+    pCardTemperatureContainer.innerText = temperature;
+    spanCardTemperatureContainer.innerText = '°C';
+    pCardTemperatureContainer.append(spanCardTemperatureContainer);
+    cardTemperatureContainer.append(pCardTemperatureContainer, spanCardTemperatureContainer);
+    
+    const cardImgWeather = document.createElement('img'),
+          cardWeatherDescription = document.createElement('p');
+    cardWeatherDescription.className = 'card--weather-description';
+
+    cardImgWeather.src = `https://openweathermap.org/img/wn/${imgIcon}@2x.png`;
+    cardImgWeather.alt = 'card image weather';
+    cardWeatherDescription.innerText = cityWeatherDescription;
+    card.append(cardImgWeather, cardWeatherDescription);
+}
 
 const getLocalParameters = async (urlApi) => {
     let valueToReturn = null;
@@ -30,6 +92,7 @@ const getLocalParameters = async (urlApi) => {
 
     return valueToReturn;
 }
+
 
 const getWeather = async (latitude, longitude) => {
     let weatherToReturn = null;
@@ -55,14 +118,19 @@ btn.addEventListener('click', () => {
         latitude = res[0];
         longitude = res[1];
 
-        let hola = getWeather(latitude, longitude);
-        console.log(hola);
-        return hola;
+        let latitudeAndLongitude = getWeather(latitude, longitude);
+        console.log(latitudeAndLongitude);
+
+        return latitudeAndLongitude;
     })
     .then((data) => {
-        const temperatureInKelvin = data.main.temp - 273.15;
+        const temperature = Math.floor(data.main.temp - 273.15);
+        const cityName = data.name;
+        const cityCountry = data.sys.country;
+        const imgIcon = data.weather[0].icon;
+        const cityWeatherDescription = data.weather[0].description;
 
-        p.innerHTML = temperatureInKelvin;
+        buildCard(temperature, cityName, cityCountry, imgIcon, cityWeatherDescription);
     });
         
 });
